671ec60bfdfa0dda5047263e13f44e298ecfaa24
haziq-aziz <hxziqaziz@gmail.com>
2024-12-18 17:15:13 +0800

    Added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Sparkles } from "@react-three/drei";
-import { useRef } from "react";
-
-const RotatingCube = () => {
-  const meshRef = useRef();
-  
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  })
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="#468585" emissive="#468585" />
-
-      <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.2} color="orange" />
-    </mesh>
-  )
-}
+import Navbar from "./sections/Navbar";
 
 const App = () => {
-  return (
-    <Canvas style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
-      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
-      <color attach="background" args={["#F0F0F0"]} />
-
-      <RotatingCube />
-    </Canvas>
-  ) 
-}
+  return <main className="max-w-7xl mx-auto">
+    <Navbar />
+  </main>;
+};
 
 export default App;
